46d7bf430e068677d874a398fa930014beb3c3d9
Veera Sankar <sankarsivam6991@gmail.com>
2024-10-17 00:03:02 +0530

    add user and profile  updated

--- src/presentation/pages/user/addUserDetails.js
+++ src/presentation/pages/user/addUserDetails.js
@@ -16,8 +16,6 @@ function AddUserDetails({ onUserAdded }) {
     { label: "Settlement" },
   ]);
 
-  
-
   const onClickAddUser = () => {
     if (!userName || !email || !mobileNo || !role) {
       enqueueSnackbar("Please fill out allfields", { variant: "error" });
@@ -29,8 +27,7 @@ function AddUserDetails({ onUserAdded }) {
       });
       return;
     }
-    
-    
+
     setLoading(true);
 
     ApiRequestPost.addUser(userName, email, role)
@@ -57,64 +54,93 @@ function AddUserDetails({ onUserAdded }) {
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <CustomInput
-        type="input"
-        value={userName}
-        onChange={(value) => setUserName(value)}
-        placeholder="Name"
-        validator="name"
-        isMandatory={true}
-        error={"Enter valid Name"}
-        inputStyles={{
-          width:"350px",
-          padding: "10px",
-          borderRadius: "4px",
-          margin: "10px",
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+        marginBottom: 40,
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          height: "70%",
+          width: "100%",
+          flexWrap: "wrap",
+          marginTop: 20,
+          alignItems: "center",
+          justifyContent: "center",
         }}
-      />
-      <CustomInput
-        type="input"
-        value={email}
-        onChange={(value) => setEmail(value)}
-        placeholder="Email"
-        validator="email"
-        error={"Enter valid Email"}
-        isMandatory={true}
-        inputStyles={{
-          width:"350px",
-          padding: "10px",
-          borderRadius: "4px",
-          margin: "10px",
-        }}
-      />
-      <CustomInput
-        type="input"
-        value={mobileNo}
-        onChange={(value) => setMobileNo(value)}
-        placeholder="Mobile No."
-        validator="mobile"
-        error={"* Enter Valid Mobile No."}
-        isMandatory={true}
-        inputStyles={{
-          width:"350px",
-          padding: "10px",
-          borderRadius: "4px",
-          margin: "10px",
-        }}
-      />
+      >
+        <CustomInput
+          type="input"
+          value={userName}
+          onChange={(value) => setUserName(value)}
+          placeholder="Name"
+          validator="name"
+          isMandatory={true}
+          error={"Enter valid Name"}
+          inputStyles={{
+            width: "350px",
+            padding: "10px",
+            borderRadius: "4px",
+            margin: "10px",
+          }}
+        />
+        <CustomInput
+          type="input"
+          value={email}
+          onChange={(value) => setEmail(value)}
+          placeholder="Email"
+          validator="email"
+          error={"Enter valid Email"}
+          isMandatory={true}
+          inputStyles={{
+            width: "350px",
+            padding: "10px",
+            borderRadius: "4px",
+            margin: "10px",
+          }}
+        />
+        <CustomInput
+          type="input"
+          value={mobileNo}
+          onChange={(value) => setMobileNo(value)}
+          placeholder="Mobile No."
+          validator="mobile"
+          error={"* Enter Valid Mobile No."}
+          isMandatory={true}
+          inputStyles={{
+            width: "350px",
+            padding: "10px",
+            borderRadius: "4px",
+            margin: "10px",
+          }}
+        />
 
-      <CustomSelectComponenet
-        value={role}
-        onChange={(event) => {
-          setRole(event.target.value);
-        }}
-        data={roleDropdown}
-        label="Role"
-        style={{ width: 370, margin: 10 }}
-      />
+        <CustomSelectComponenet
+          value={role}
+          onChange={(event) => {
+            setRole(event.target.value);
+          }}
+          data={roleDropdown}
+          label="Role"
+          style={{ width: 370, margin: 10 }}
+        />
+      </div>
 
-      <div style={{ width: "50%", display: "flex", justifyContent: "center", margin: 10,padding:10 }}>
+      <div
+        style={{
+          width: "50%",
+          display: "flex",
+          justifyContent: "center",
+          margin: 10,
+          padding: 10,
+        }}
+      >
         <ButtonComponent label={"Submit"} onClick={onClickAddUser} />
       </div>
     </div>
